1374eeba673dadfbf6d9cdcae74ea465672b7624
Sat1906 <rsathishkumar1963@gmail.com>
2022-08-27 07:24:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-import hikari
-bot = hikari.GatewayBot(token="MTAxMjA2MTUwNjc5MzM5NDE4OA.GApTeg.Yq5Lv5eg0CpSp0XTRq-on7uoDETrcNb1qBijO0")
-bot.run
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('MTAxMjA2MTUwNjc5MzM5NDE4OA.GbbvS7.8awBrft-_4ev4yYNJEBc6Ss-02LAFKWGNn9wg8')
